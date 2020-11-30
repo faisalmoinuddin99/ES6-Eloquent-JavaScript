8c977b78197b353ccca4670dd1feac6ba1b9807e
faisal <faisal25march99@gmail.com>
2020-11-30 11:56:16 +0530

    Triangle Pattern Problem

---
+++ Excercise/1.js
@@ -0,0 +1,19 @@
+/*
+Write a loop that makes seven calls to console.log to output the
+ following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
+let i = 0
+let num = 7
+while(i <= num){
+    console.log("#".repeat(i));
+i++
+}
